d6984e3fafd9b0b46997e74bbefded446f94413f
409-11-sansern <54191@yupparaj.ac.th>
2026-08-04 14:08:18 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นายสรรเสริญ สืทายาท เลขที่ 11 ม.4/9")
+st.write("นายสรรเสริญ สืทายาท เลขที่ 11  ม.4/9")
